bed9d83f67590e9a54c43508b89622b5db9cbd2e
hellozcr <83883801+hellozcr@users.noreply.github.com>
2023-03-28 01:51:12 +0530

    Create collegeAddress.py

---
+++ collegeAddress.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup as bs
+
+url1 = 'https://www.acharya.ac.in/'
+
+page = requests.get(url1)
+
+soup = bs(page.content,'html.parser')
+add = soup.find(attrs={'class': 'col-md-4 col-sm-12 ftr-item'}).find_all('p')
+# print(len(add))
+
+for i in range(1,5):
+    add2 = str(add[i])
+    add3 = add2.replace('<p>'," ")
+    add4 = add3.replace('</p>'," ")
+    print(add4)
+
+  

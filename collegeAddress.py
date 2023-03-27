58762dda147707794cfc21a42815d072d0796ee1
hellozcr <83883801+hellozcr@users.noreply.github.com>
2023-03-28 01:58:31 +0530

    Update collegeAddress.py

--- collegeAddress.py
+++ collegeAddress.py
@@ -3,19 +3,23 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup as bs
 
+
 def collegeAdd(urlx):
-url1 = 'https://www.acharya.ac.in/'
+    url1 = urlx
 
-page = requests.get(url1)
 
-soup = bs(page.content,'html.parser')
-add = soup.find(attrs={'class': 'col-md-4 col-sm-12 ftr-item'}).find_all('p')
-# print(len(add))
+    page = requests.get(url1)
 
-for i in range(1,5):
-    add2 = str(add[i])
-    add3 = add2.replace('<p>'," ")
-    add4 = add3.replace('</p>'," ")
-    print(add4)
+    soup = bs(page.content, 'html.parser')
+    add = soup.find(attrs={'class': 'col-md-4 col-sm-12 ftr-item'}).find_all('p')
+ 
 
-  
+    for i in range(1, 6):
+        add2 = str(add[i])
+        add3 = add2.replace('<p>', " ")
+        add4 = add3.replace('</p>', " ")
+        return add4
+
+
+url = 'https://www.acharya.ac.in/'
+print(f"College Address: {collegeAdd(url)}")
